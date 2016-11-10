eedaf994db5be490f012326e8e83960629fc4bd5
junhai <466860042@qq.com>
2016-11-10 13:22:33 +0800

    add header

--- MonkeyMapConfig/MMKLocationConfig.h
+++ MonkeyMapConfig/MMKLocationConfig.h
@@ -23,7 +23,9 @@
 //使用百度地图检索
 #import <BaiduMapAPI_Base/BMKBaseComponent.h>
 #import <BaiduMapAPI_Location/BMKLocationComponent.h>
-#import <BaiduMapAPI_Search/BMKSearchComponent.h>
+#import <BaiduMapAPI_Search/BMKSearchComponent.h> 
+#import <BaiduMapAPI_Map/BMKMapComponent.h>
+#import <BaiduMapAPI_Utils/BMKUtilsComponent.h>
 
 
 #define __MMKReverseGeoCodeResult       BMKReverseGeoCodeResult
